75345ce57557554f39014327403790226c0c4120
PittawasChoo <pittawas.boom@gmail.com>
2025-03-07 12:09:53 +0700

    create community options constant

---
+++ src/constants/communityOptions.js
@@ -0,0 +1,20 @@
+export const FILTER_COMMUNITY_OPTIONS = [
+    "All",
+    "History",
+    "Food",
+    "Pets",
+    "Health",
+    "Fashion",
+    "Exercise",
+    "Others",
+];
+
+export const COMMUNITY_OPTIONS = [
+    "History",
+    "Food",
+    "Pets",
+    "Health",
+    "Fashion",
+    "Exercise",
+    "Others",
+];
