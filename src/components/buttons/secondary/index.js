663dde0d7612c2226c203abbd99f4d82575d95d5
PittawasChoo <pittawas.boom@gmail.com>
2025-03-07 12:09:04 +0700

    create secondary button component

---
+++ src/components/buttons/secondary/index.js
@@ -0,0 +1,23 @@
+import { IBM_Plex_Sans_Thai } from "next/font/google";
+import "./styles.css";
+
+const IBMPlexSansThai = IBM_Plex_Sans_Thai({
+    variable: "--font-ibm-plex-sans-thai",
+    subsets: ["latin", "thai"],
+    weight: "500",
+});
+
+const SecondaryButton = ({ children, className, loading, onClick }) => {
+    return (
+        <button
+            type="submit"
+            className={`btn btn-outline-success ${IBMPlexSansThai.className} ${className}`}
+            onClick={onClick}
+        >
+            {loading && <span className="spinner-border spinner-border-sm" aria-hidden="true" />}
+            {children}
+        </button>
+    );
+};
+
+export default SecondaryButton;
