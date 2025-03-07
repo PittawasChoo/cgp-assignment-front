e5ba83510e879bae7bc6c34778769f080378ead9
PittawasChoo <pittawas.boom@gmail.com>
2025-03-07 14:13:19 +0700

    post and comment page

--- src/app/post/page.js
+++ src/app/post/page.js
@@ -1,47 +1,72 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
-import { useState, useEffect } from "react";
+
+import Loading from "components/loading";
+import Navbar from "components/navbar";
+import PostDetail from "./postDetail";
+import Sidebar from "components/sidebar";
+
+import "./styles.css";
 
 export default function PostPage() {
+    const [post, setPost] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
     const searchParams = useSearchParams();
     const postId = searchParams.get("id");
-    const [post, setPost] = useState({});
-    // const [loading, setLoading] = useState(true);
-    // const [error, setError] = useState("");
+
+    const fetchPost = async () => {
+        if (!postId) return;
+
+        setLoading(true);
+        try {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/post/${postId}`);
+            if (!res.ok) throw new Error("Failed to fetch post");
+
+            const data = await res.json();
+            setPost(data);
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     useEffect(() => {
-        if (!postId) {
-            setError("Post ID is missing");
-            setLoading(false);
-            return;
-        }
-
-        const fetchPost = async () => {
-            try {
-                const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/post/${postId}`);
-                if (!res.ok) throw new Error("Failed to fetch post");
-
-                const data = await res.json();
-                setPost(data);
-            } catch (err) {
-                // setError(err.message);
-            } finally {
-                // setLoading(false);
-            }
-        };
-
         fetchPost();
     }, [postId]);
 
-    //   if (loading) return <p>Loading...</p>;
-    //   if (error) return <p className="text-red-500">{error}</p>;
+    if (!error) return <ErrorPage refetch={fetchPost} />;
 
     return (
-        <div className="p-6">
-            <h1 className="text-2xl font-bold">{post.title}</h1>
-            <p>{post.detail}</p>
-            <small className="text-gray-500">Community: {post.community}</small>
+        <div className="board-root">
+            <Navbar />
+            <div className="board-container">
+                <div style={{ width: "280px", minWidth: "280px" }}>
+                    <Sidebar />
+                </div>
+
+                <div
+                    style={{
+                        flexShrink: 1,
+                        minHeight: "calc(100vh - 60px)",
+                        flex: 1,
+                        paddingTop: "36px",
+                        backgroundColor: "var(--white)",
+                        display: "flex",
+                        justifyContent: "center",
+                    }}
+                >
+                    {loading ? (
+                        <Loading />
+                    ) : (
+                        <PostDetail postId={postId} post={post} fetchPost={fetchPost} />
+                    )}
+                </div>
+            </div>
         </div>
     );
 }
---
+++ src/app/post/postDetail/index.js
@@ -0,0 +1,295 @@
+"use client";
+
+import { Formik, Form } from "formik";
+import { Inter } from "next/font/google";
+import { useContext, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import UserImage from "components/userImage";
+import Badge from "components/badge";
+import PrimaryButton from "components/buttons/primary";
+
+import { AuthContext } from "context/authContext";
+
+import { timeAgo } from "@/utils/timeAgo";
+
+import "./styles.css";
+import SecondaryButton from "components/buttons/secondary";
+import { validationSchema } from "./validationSchema";
+
+const InterFont = Inter({
+    variable: "--font-inter",
+    subsets: ["latin"],
+    weight: "400",
+});
+
+const SemiBoldInterFont = Inter({
+    variable: "--font-inter",
+    subsets: ["latin"],
+    weight: "500",
+});
+
+const BoldInterFont = Inter({
+    variable: "--font-inter",
+    subsets: ["latin"],
+    weight: "600",
+});
+
+const PostDetail = ({ postId, post, fetchPost }) => {
+    const [showCommentField, setShowCommentField] = useState(false);
+    const [detail, setDetail] = useState("");
+    const [fetching, setFetching] = useState(false);
+    const [fetchingError, setFetchingError] = useState("");
+
+    const router = useRouter();
+
+    const { username } = useContext(AuthContext);
+
+    const onSubmit = async (values) => {
+        const { detail } = values;
+
+        setFetching(true);
+        setFetchingError("");
+
+        try {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/comment`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + localStorage.getItem("authToken") || "",
+                },
+                body: JSON.stringify({ postId: postId, detail }),
+            });
+
+            const data = await res.json();
+
+            if (!res.ok) {
+                throw new Error(data.message || "Comment failed");
+            }
+
+            setShowCommentField(false);
+            setDetail("");
+            fetchPost();
+        } catch (err) {
+            setFetchingError(err.message);
+        } finally {
+            setFetching(false);
+        }
+    };
+
+    const initialValues = {
+        detail: "",
+    };
+
+    return (
+        <div style={{ width: 760, padding: "0 20px" }}>
+            <div
+                style={{
+                    width: "44px",
+                    height: "44px",
+                    marginBottom: "40px",
+                    backgroundColor: "var(--green100)",
+                    borderRadius: "50%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    cursor: "pointer",
+                }}
+                onClick={() => router.push("/")}
+            >
+                <img src="/arrow-left.png" alt="go back" width={24} height={24} />
+            </div>
+            <div
+                style={{
+                    display: "flex",
+                    gap: "10px",
+                    alignItems: "center",
+                    marginBottom: "10px",
+                }}
+            >
+                <UserImage username={post.createdBy} size="xl" />
+                <span
+                    className={`${SemiBoldInterFont.className}`}
+                    style={{ color: "var(--text)", fontSize: "14px" }}
+                >
+                    {post.createdBy}
+                </span>
+                <span
+                    className={`${InterFont.className}`}
+                    style={{ color: "var(--grey300)", fontSize: "12px" }}
+                >
+                    {timeAgo(post.timestamp._seconds)}
+                </span>
+            </div>
+            <Badge community={post.community} />
+            <div
+                className={`${BoldInterFont.className}`}
+                style={{
+                    fontSize: "16px",
+                    lineHeight: "24px",
+                    marginTop: "16px",
+                    color: "#101828",
+                }}
+            >
+                {post.title}
+            </div>
+            <div
+                className={`${InterFont.className}`}
+                style={{
+                    fontSize: "12px",
+                    lineHeight: "14px",
+                    marginTop: "16px",
+                    color: "var(--text)",
+                }}
+            >
+                {post.detail}
+            </div>
+            {post.comments.length > 0 && (
+                <div
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        marginTop: "28px",
+                    }}
+                >
+                    <img src="/comment.png" width={16} height={16} alt="comment" />
+                    <span
+                        className={`${InterFont.className}`}
+                        style={{
+                            marginLeft: "5px",
+                            fontSize: "12px",
+                            color: "var(--grey300)",
+                        }}
+                    >
+                        {post.comments.length} Comments
+                    </span>
+                </div>
+            )}
+
+            <div style={{ marginTop: "32px" }}>
+                {showCommentField ? (
+                    <Formik
+                        enableReinitialize
+                        initialValues={initialValues}
+                        validationSchema={validationSchema}
+                        onSubmit={(values) => {
+                            onSubmit({
+                                detail: values.detail,
+                            });
+                        }}
+                    >
+                        {(formProps) => (
+                            <Form>
+                                <div style={{ marginBottom: "10px" }}>
+                                    <textarea
+                                        name="detail"
+                                        className={`form-control post-text-area ${
+                                            formProps.touched["detail"] &&
+                                            formProps.errors["detail"] &&
+                                            "is-invalid"
+                                        }`}
+                                        placeholder="What's on your mind..."
+                                        value={detail}
+                                        onChange={(e) => {
+                                            formProps.setFieldValue("detail", e.target.value);
+                                            setDetail(e.target.value);
+                                        }}
+                                    />
+                                    <div id="validationDetail" className="invalid-feedback">
+                                        Please provide post comment.
+                                    </div>
+                                </div>
+                                {fetchingError && (
+                                    <div className="bg-danger text-white p-3 error-box">
+                                        {fetchingError}
+                                    </div>
+                                )}
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        justifyContent: "flex-end",
+                                        gap: "12px",
+                                    }}
+                                >
+                                    <SecondaryButton onClick={() => setShowCommentField(false)}>
+                                        Cancel
+                                    </SecondaryButton>
+                                    <PrimaryButton loading={fetching}>Post</PrimaryButton>
+                                </div>
+                            </Form>
+                        )}
+                    </Formik>
+                ) : (
+                    <SecondaryButton
+                        className="custom-secondary-btn"
+                        onClick={() => {
+                            if (!username) {
+                                router.push("/signin");
+                            } else {
+                                setShowCommentField(true);
+                            }
+                        }}
+                    >
+                        Add Comment
+                    </SecondaryButton>
+                )}
+            </div>
+
+            {post.comments.length > 0 && (
+                <div style={{ marginTop: "24px" }}>
+                    {post.comments.map((comment, index) => (
+                        <div
+                            key={comment.id}
+                            style={{
+                                display: "flex",
+                                gap: "10px",
+                                marginBottom: "24px",
+                            }}
+                        >
+                            <div style={{ width: "40px" }}>
+                                <UserImage username={comment.createdBy} />
+                            </div>
+                            <div>
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        height: "40px",
+                                        alignItems: "center",
+                                        gap: "10px",
+                                    }}
+                                >
+                                    <div
+                                        className={`${SemiBoldInterFont.className}`}
+                                        style={{
+                                            fontSize: "14px",
+                                            color: "var(--text)",
+                                        }}
+                                    >
+                                        {comment.createdBy}
+                                    </div>
+                                    <div
+                                        className={`${InterFont.className}`}
+                                        style={{
+                                            fontSize: "12px",
+                                            color: "var(--grey300)",
+                                        }}
+                                    >
+                                        {timeAgo(comment.timestamp._seconds)}
+                                    </div>
+                                </div>
+                                <div
+                                    className={`${InterFont.className}`}
+                                    style={{ fontSize: "12px", color: "var(--text)" }}
+                                >
+                                    {comment.detail}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default PostDetail;
